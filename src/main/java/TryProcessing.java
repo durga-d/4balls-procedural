1b618d422e56455b4478d376fc20072046d03f60
durga-d <durgadinakaran01@gmail.com>
2022-03-29 14:18:02 +0530

    updated code smells

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -4,7 +4,8 @@ public class TryProcessing extends PApplet {
     public static final int WIDTH=640;
     public static final int HEIGHT=640;  // magic numbers - code smells
     private static final int DIAMETER=20;
-    int x=0;int y=0;int z=0;int a=0;
+    int speed1=0,speed2=0,speed3=0,speed4=0;
+    public static final int H1=HEIGHT/5,H2=2*HEIGHT/5,H3=3*HEIGHT/5,H4=4*HEIGHT/5;
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
@@ -30,14 +31,14 @@ public class TryProcessing extends PApplet {
     }
     private void drawCircle(){
 //        ellipse(mouseX, mouseY, DIAMETER, DIAMETER);
-        ellipse(x, HEIGHT/5, DIAMETER, DIAMETER);
-        x++;
-        ellipse(y, 2*HEIGHT/5, DIAMETER, DIAMETER);
-        y+=2;
-        ellipse(z, 3*HEIGHT/5, DIAMETER, DIAMETER);
-        z+=3;
-        ellipse(a, 4*HEIGHT/5, DIAMETER, DIAMETER);
-        a+=4;
+        ellipse(speed1, H1, DIAMETER, DIAMETER);
+        speed1++;
+        ellipse(speed2, H2, DIAMETER, DIAMETER);
+        speed2+=2;
+        ellipse(speed3, H3, DIAMETER, DIAMETER);
+        speed3+=3;
+        ellipse(speed4, H4, DIAMETER, DIAMETER);
+        speed4+=4;
 //        ellipse(x, mouseY, DIAMETER, DIAMETER);
 //        x++;
     }
